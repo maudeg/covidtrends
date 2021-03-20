3f365e882fac4652aa28ee9bb123cf3c89f9eb64
Maurizio de Gregorio <maui.deg@gmail.com>
2021-03-20 12:19:15 +0100

    added Rt script

---
+++ deploy-rt.sh
@@ -0,0 +1,18 @@
+echo "download new input data"
+# https://github.com/ondata/iss-epicentro-rt-nazionale
+datecsv='2021-03-03' 
+# download csv at date
+curl https://raw.githubusercontent.com/ondata/iss-epicentro-rt-nazionale/main/curva_epidemica_Italia_$datecsv -o ./data/curva-epidemica/curva_epidemica_Italia_$datecsv.txt
+# update latest
+cp ./data/curva-epidemica/curva_epidemica_Italia_$datecsv.txt ./data/curva-epidemica/curva_epidemica_Italia_latest.txt
+# generate Rt
+echo "Generate Rt data..."
+/Library/Frameworks/R.framework/Resources/bin/R --no-save -f ./algorithm/rt/r/calcoloRt.R
+# copy /tmp/rtdata.csv on github latest
+# commit/push master
+cp /tmp/rtdata.csv ./data/rt-italia/rt-italia-$datecsv.csv
+cp /tmp/rtdata.csv ./data/rt-italia/rt-italia-latest.csv
+# echo "Upload rtdata.csv on S3..."
+# aws s3 cp /tmp/rtdata.csv s3://covid.com/ --acl public-read --cache-control "no-cache" --content-type "text/plain"
+# echo "AWS CDN invalidation..."
+# aws cloudfront create-invalidation --distribution-id E331KP4314704 --paths "/rtdata.csv"
